020fc7dfb48970b3b384dcd00e6ee30da406f25e
EC2 Default User <ec2-user@ip-172-31-6-1.ap-northeast-1.compute.internal>
2022-09-07 01:33:59 +0000

    [Update]2-6-1 解答を追加

--- lesson6-1.rb
+++ lesson6-1.rb
@@ -8,3 +8,14 @@ total = a * b
 puts "計算結果を出力します"
 puts "a*b=#{total}"
 puts "計算を終了します"
+
+# 解答↓
+puts "計算をはじめます"
+puts "2つの値を入力してください"
+
+a = gets.to_i
+b = gets.to_i
+
+puts "計算結果を出力します"
+puts "a*b=#{a * b}"
+puts "計算を終了します"
